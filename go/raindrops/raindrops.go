2835d45a1566be523f7f0e25e8d932f04a867028
obitech <ajknipping@mailbox.org>
2019-12-26 20:33:42 +0100

    Improve raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -6,19 +6,18 @@ import "strconv"
 // Convert returns a different string, depending if input is evenly divisible
 // through 3, 5 and/or 7.
 func Convert(input int) string {
-	by3, by5, by7 := input%3 == 0, input%5 == 0, input%7 == 0
-	if by3 || by5 || by7 {
-		var out string
-		if by3 {
-			out += "Pling"
-		}
-		if by5 {
-			out += "Plang"
-		}
-		if by7 {
-			out += "Plong"
-		}
-		return out
+	var out string
+	if input%3 == 0 {
+		out += "Pling"
 	}
-	return strconv.Itoa(input)
+	if input%5 == 0 {
+		out += "Plang"
+	}
+	if input%7 == 0 {
+		out += "Plong"
+	}
+	if out == "" {
+		return strconv.Itoa(input)
+	}
+	return out
 }
